8f40a3abf0f5071f90366f7bd880e05ba98bd14e
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-01-31 23:24:00 +0530

    UI & backend integreated

--- backend/src/config/validation.ts
+++ backend/src/config/validation.ts
@@ -4,6 +4,8 @@ export const validationRules = {
     amount: { min: 0 },
     dateValidation: (date: Date): boolean => {
       const now = new Date();
+      console.log("hyy there");
       return date.getMonth() === now.getMonth() && date.getFullYear() === now.getFullYear();
     }
   };
+  // hyyyy
